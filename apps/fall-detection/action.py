85fb9f5bc88e7eb52f556c0270a75cb18a4d2548
Jayaraj Poroor <jayaraj.poroor@paanini.com>
2020-11-17 16:31:59 +0530

    fall detection working

--- apps/fall-detection/action.py
+++ apps/fall-detection/action.py
@@ -1,7 +1,13 @@
+EVENT_EXPIRY_MILLIS: const uint64_t = 4000
+last_ts: uint64_t
+
 def init() -> void:
     pass
 
 @flow
 def do() -> void:
-    Serial.println("Fall detected!")
-    delay(1000)
+    current_ts: uint64_t
+    current_ts = millis()
+    if (current_ts - last_ts) > EVENT_EXPIRY_MILLIS:
+        Serial.println("Fall detected!")
+    last_ts = current_ts
